0be84df193c0aa0f729ae54780b48bda65738be4
diecastscape <mhaflal85@gmail.com>
2026-01-27 13:17:38 +0530

    Update sale-config.js

--- sale-config.js
+++ sale-config.js
@@ -1,9 +1,8 @@
-<script>
+
   /* 🔥 SINGLE SOURCE OF TRUTH */
 
   const SALE_CONFIG = {
     enabled: false, // true = force ON, false = auto by time
     start: "2026-01-25T10:00:00", // IST
-  };
-</script>
+  }
 
