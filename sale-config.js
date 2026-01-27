b7d4bf91631a355701262ef6a48f2eb159a1e136
diecastscape <mhaflal85@gmail.com>
2026-01-27 12:59:40 +0530

    Create sale-config.js

---
+++ sale-config.js
@@ -0,0 +1,9 @@
+<script>
+  /* 🔥 SINGLE SOURCE OF TRUTH */
+
+  const SALE_CONFIG = {
+    enabled: false, // true = force ON, false = auto by time
+    start: "2026-01-25T10:00:00", // IST
+  };
+</script>
+
