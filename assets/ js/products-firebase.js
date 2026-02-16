0d5e1beae53d3418fde4cfa03d0ab6682e8a166b
diecastscape <mhaflal85@gmail.com>
2026-02-16 19:30:36 +0530

    Update products-firebase.js

--- assets/ js/products-firebase.js
+++ assets/ js/products-firebase.js
@@ -23,7 +23,7 @@ const imgs = p.images.map(im => `
 
   const whatsappText =
     encodeURIComponent(
-      `Hi Diecast.scape,\nI would like to place an order for the ${p.name}. priced at ₹${p.priceNew} (${discount}% discount). Kindly let me know the delivery options and payment details.`
+      `Hi Diecast.scape,\nI would like to place an order for the ${p.name}\npriced at ₹${p.priceNew} (${discount}% discount). \nKindly let me know the delivery options and payment details.`
     );
 
   return `
