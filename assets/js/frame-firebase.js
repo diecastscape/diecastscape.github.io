f55b660b3897325bbe51c06bb5e1323034a365f9
diecastscape <mhaflal85@gmail.com>
2026-07-07 14:05:24 +0530

    Create frame-firebase.js

---
+++ assets/js/frame-firebase.js
@@ -0,0 +1,157 @@
+import { db } from "./firebase-init.js";
+import { 
+  collection,
+  query,
+  orderBy,
+  getDocs,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+function buildSaleHTML(p){
+
+  let imgs = "";
+
+  if(Array.isArray(p.images)){
+    p.images.forEach(im=>{
+      imgs += `
+        <div class="img-box">
+          <div class="img-loader"></div>
+          <img src="/images/accessories/${im}.webp"
+            onload="this.previousElementSibling.remove(); this.style.opacity=1"
+            style="opacity:0"
+            onclick="openLightbox(this.src)">
+        </div>
+      `;
+    });
+  }
+
+  return `
+  <div class="section">
+    
+    <div class="slider">
+      ${imgs}
+   </div>
+<div class="diorama-title">${p.name}</div>
+
+    <div class="price">
+      <span class="new">₹${p.price}/-</span>
+    </div>
+<div class="cart-controls">
+
+<button
+class="qty-btn"
+onclick="
+addProductInfo(
+'${p.id}',
+'${p.name}',
+${p.price}
+);
+changeQty('${p.id}',-1);
+">
+
+−
+
+</button>
+
+<span
+class="qty"
+id="qty-${p.id}">
+
+0
+
+</span>
+
+<button
+class="qty-btn"
+onclick="
+addProductInfo(
+'${p.id}',
+'${p.name}',
+${p.price}
+);
+changeQty('${p.id}',1);
+">
+
++
+
+</button>
+    </div>
+  </div>
+  `;
+}
+function restoreCart() {
+
+  Object.keys(cart).forEach(id => {
+
+    const qtyBox = document.getElementById("qty-" + id);
+
+    if (qtyBox) {
+      qtyBox.innerText = cart[id].qty;
+    }
+
+  });
+
+  updateCartBar();
+
+}
+async function loadSaleProducts(){
+
+  const container =
+    document.getElementById("sale-main");
+
+  const loader =
+    document.getElementById("productsLoader");
+
+  if(!container) return;
+
+  // ===== LOAD PRODUCTS DIRECTLY =====
+  const q = query(
+    collection(db,"accessoriesProducts"),
+    orderBy("created","desc")
+  );
+
+  const snap = await getDocs(q);
+
+  let count = 0;
+
+  snap.forEach(doc=>{
+
+    const p = doc.data();
+     p.id = doc.id;
+    if(p.active === true){
+
+      container.insertAdjacentHTML(
+        "beforeend",
+        buildSaleHTML(p)
+      );
+
+      count++;
+    }
+
+  });
+
+  // Remove loader
+  requestAnimationFrame(()=>{
+    if(loader) loader.remove();
+  });
+restoreCart();
+  // Empty state
+  if(count===0){
+
+    container.innerHTML=`
+      <div class="border-top">
+        <div class="sale-off">
+          <p>No products available</p>
+        </div>
+      </div>
+    `;
+
+  }
+
+}
+
+window.addEventListener(
+  "DOMContentLoaded",
+  loadSaleProducts
+);
