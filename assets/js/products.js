6fb8f8e15b0effbbdcce6fd7127016a223b6bc63
diecastscape <mhaflal85@gmail.com>
2026-08-07 12:39:02 +0530

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -58,19 +58,3 @@ window.addEventListener("popstate", function () {
   if (lightbox) lightbox.style.display = "none";
 });
 
-function openDetailsSheet(btn) {
-  const details = btn.nextElementSibling; // .product-details
-  const content = details.innerHTML;
-
-  document.getElementById("sheetContent").innerHTML = content;
-  document.getElementById("sheetOverlay").classList.add("show");
-  document.getElementById("bottomSheet").classList.add("show");
-
-  document.body.style.overflow = "hidden"; // lock background scroll
-}
-
-function closeSheet() {
-  document.getElementById("sheetOverlay").classList.remove("show");
-  document.getElementById("bottomSheet").classList.remove("show");
-  document.body.style.overflow = "";
-}
